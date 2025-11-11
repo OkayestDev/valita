4d03d98a5ba09604a4658f286f391465aafd7a0c
Kyle Richardson <richardson.a.kyle.95@gmail.com>
2025-11-11 09:46:53 -0500

    fixing tests

--- src/handlers/__tests__/logger.handler.test.ts
+++ src/handlers/__tests__/logger.handler.test.ts
@@ -14,7 +14,7 @@ describe("loggerHandler", () => {
             };
             configureLogger({ enableRequestLogging: true });
             logRequest("test", request);
-            expect(logSpy).toHaveBeenCalledWith({ path: "test", request });
+            expect(logSpy).toHaveBeenCalledWith("test", request);
         });
 
         it("should not log a request if logging is disabled", () => {
@@ -39,7 +39,7 @@ describe("loggerHandler", () => {
             const logSpy = jest.spyOn(console, "log");
             configureLogger({ enableResponseLogging: true });
             logResponse("test", response);
-            expect(logSpy).toHaveBeenCalledWith({ path: "test", response });
+            expect(logSpy).toHaveBeenCalledWith( "test", response);
         });
 
         it("should not log a response if logging is disabled", () => {
